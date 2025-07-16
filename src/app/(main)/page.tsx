ab733908cd11b53e4390fd3340aac268939b7f98
kyeoungwoon <thsrlgns05@naver.com>
2025-07-16 07:34:47 +0000

    Deploy PR #139 from develop

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,5 @@
 const MainPage = () => {
-  return <div className="p-10">메인 페이지</div>;
+  return <div className="md:text-system-red p-10 text-center text-black">메인 페이지</div>;
 };
 
 export default MainPage;
