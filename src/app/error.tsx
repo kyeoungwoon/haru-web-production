4f2f952ea6cb130779cdfca72d566ee02644ad1f
kyeoungwoon <thsrlgns05@naver.com>
2025-08-07 13:48:05 +0000

    Deploy PR #222 from develop

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,7 +2,13 @@
 
 import { useEffect } from 'react';
 
+import Image from 'next/image';
+
+import errorImage from '@assets/images/404/image.png';
+
 import { captureRenderError } from '@lib/sentry';
+
+import CustomButton from '@buttons/38px/CustomButton/CustomButton.client';
 
 const Error = ({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) => {
   useEffect(() => {
@@ -12,10 +18,18 @@ const Error = ({ error, reset }: { error: Error & { digest?: string }; reset: ()
   }, [error]);
 
   return (
-    <>
-      <h2>예상치 못한 오류가 발생했습니다.</h2>
-      <button onClick={reset}>다시 시도하기</button>
-    </>
+    <main className="flex min-h-screen items-center justify-center">
+      <section className="gap-74pxr flex shrink-0 items-center">
+        <div className="w-658pxr h-440pxr relative">
+          <Image src={errorImage} alt="에러 이미지" fill />
+        </div>
+        <div>
+          <h2 className="mb-14pxr text-primary h-52pr text-h4-bd">일시적인 오류가 발생했어요.</h2>
+          <p className="mb-42pxr text-b1-rg text-gray-200">잠시 후 다시 시도해 주세요.</p>
+          <CustomButton onClick={reset}>새로 고침</CustomButton>
+        </div>
+      </section>
+    </main>
   );
 };
 
