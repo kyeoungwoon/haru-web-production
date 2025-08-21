cc637fe0b19fcf9d650e656d94ea5fe3e4945d9a
kyeoungwoon <thsrlgns05@naver.com>
2025-08-21 17:15:39 +0000

    Deploy PR #393 from develop

--- src/api/on-boarding/post/apis/link-instagram.ts
+++ src/api/on-boarding/post/apis/link-instagram.ts
@@ -6,11 +6,12 @@ import { WORKSPACES_ON_BOARDING_API_ENDPOINTS } from '@/api/on-boarding/api-end-
 import { LinkInstagramRequestDto, LinkInstagramResponseDto } from '@/api/on-boarding/apis.types';
 
 export const linkInstagram = async ({ workspaceId, instagramRedirectType, code }: LinkInstagramRequestDto) => {
-  const url = new URL(WORKSPACES_ON_BOARDING_API_ENDPOINTS.LINK_INSTAGRAM(workspaceId));
-  url.searchParams.append('instagramRedirectType', instagramRedirectType);
+  const query = new URLSearchParams();
+  query.append('instagramRedirectType', instagramRedirectType);
 
+  const url = `${WORKSPACES_ON_BOARDING_API_ENDPOINTS.LINK_INSTAGRAM(workspaceId)}?${query.toString()}`;
   const response = await defaultApi<BaseResponseDto<LinkInstagramResponseDto>>(
-    url.toString(),
+    url,
     {
       method: 'POST',
       headers: {
